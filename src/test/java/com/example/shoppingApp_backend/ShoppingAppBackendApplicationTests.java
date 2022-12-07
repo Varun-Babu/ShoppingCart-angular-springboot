71b76563afbd29ec4e912bb3d8f60304e4ff75a6
Varun-Babu <itsme.varun.26@gmail.com>
2022-12-07 20:20:26 +0530

    initial commit

---
+++ src/test/java/com/example/shoppingApp_backend/ShoppingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
